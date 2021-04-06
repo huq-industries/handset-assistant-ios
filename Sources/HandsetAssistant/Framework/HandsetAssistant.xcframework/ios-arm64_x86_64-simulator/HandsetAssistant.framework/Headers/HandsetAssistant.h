705b7859d3eca675d278e1f100beb6e1392a7418
Isambard Poulson <isambard@isambard.com>
2021-04-06 12:52:46 +0100

    Add Framework

---
+++ Sources/HandsetAssistant/Framework/HandsetAssistant.xcframework/ios-arm64_x86_64-simulator/HandsetAssistant.framework/Headers/HandsetAssistant.h
@@ -0,0 +1,18 @@
+//
+//  HandsetAssistant.h
+//  HandsetAssistant
+//
+//  Created by Isambard Poulson on 17/03/2021.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for HandsetAssistant.
+FOUNDATION_EXPORT double HandsetAssistantVersionNumber;
+
+//! Project version string for HandsetAssistant.
+FOUNDATION_EXPORT const unsigned char HandsetAssistantVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <HandsetAssistant/PublicHeader.h>
+
+
